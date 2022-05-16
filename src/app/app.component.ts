a5cc482d9eba534396566d30a2871f26956451aa
pavelpiha <pavelpihagithub@gmail.com>
2022-05-16 17:11:52 +0300

    fix: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-semantic-release';
+  title = 'test-semantic-release-fix';
 }
