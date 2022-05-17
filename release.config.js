b96cc9d113c7b8ec5a843ba496732e18b56d86fb
pavelpiha <pavelpihagithub@gmail.com>
2022-05-17 18:01:38 +0300

    chore: update configs

--- release.config.js
+++ release.config.js
@@ -25,10 +25,6 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
-      {
-        changelogTitle:
-          "# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.",
-      },
     ],
     [
       "@semantic-release/npm",
@@ -52,7 +48,7 @@ module.exports = {
           "README.md",
         ],
         message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
       },
     ],
   ],
